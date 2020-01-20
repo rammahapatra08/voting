c8cce0219408e4cdd3e0ea03cafe64d63b9c64b8
IvanNardini <48505268+IvanNardini@users.noreply.github.com>
2020-01-20 05:52:04 +0100

    Update swarm_app1.sh

--- swarm_app1.sh
+++ swarm_app1.sh
@@ -1,13 +1,14 @@
 #! bin/bash
 
 # Create network
-docker nertwork create -d overlay frontend
-docker nertwork create -d overlay backend 
+docker network create -d overlay frontend
+docker network create -d overlay backend 
 
 ## Create services
 
-## Vote
-docker service create --name vote bretfisher/examplevotingapp_vote --replicas 2 --port 80:80 --network frontend
+
+## Vote --replicas 2
+docker service create --name vote bretfisher/examplevotingapp_vote --replicas 1 --port 80:80 --network frontend
 
 ## redis
 docker service create --name redis redis:3.2 --replicas 1 --network frontend
@@ -17,3 +18,6 @@ docker service create --name db postgres:9.4 --replicas 1 --network backend --mo
 
 ## worker
 docker service create --name worker bretfisher/examplevotingapp_worker:java --replicas 1 --network frontend --network frontend
+
+## result
+docker service create --name result bretfisher/examplevotingapp_result --replicas 1 --network backend -p 5001:80
